e6815662cd540d3461e142220b376e86fe7f8814
art-is-0 <icepvpking@gmail.com>
2022-08-31 13:00:48 +0200

    Changes in code

--- Math/Rekker_og_Lister.py
+++ Math/Rekker_og_Lister.py
@@ -4,9 +4,20 @@ n = 1
 an = 3
 extra = 6
 
+# For en reklusiv formel
 
-while n < 20:
+
+'''''''''
+for i in range(0,10):
     print(f"Figurtall nr {n} er {an}")
     an = an + extra
     extra = extra + 3 
     n = n + 1
+'''''''''
+
+# For en eksplisit formel
+
+for i in range(1,10):
+    def eks(x):
+        return x*2-1
+    print(f"Figurtall nr {i} er {eks(i)}")
